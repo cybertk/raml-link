34fa75c6b56e3553b5a97b5b4032faaae37325e3
Quanlong He <kyan.ql.he@gmail.com>
2014-10-13 22:56:19 +0800

    Redirct github for '/'

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ var cors = require('cors')
 var app = module.exports = express();
 
 // Openshift first
-host = process.env.OPENSHIFT_NODEJS_IP || process.env.HOST || '0.0.0.0'
-port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000
+host = process.env.OPENSHIFT_NODEJS_IP || process.env.HOST || '0.0.0.0';
+port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000;
 
-app.use(cors())
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'ramls')));
+
+app.get('/', function(req, res) {
+  res.redirect('https://github.com/cybertk/raml-link')
+});
 
 app.listen(port, host, function () {
   console.log( "Listening on " + host + ":" + port )
